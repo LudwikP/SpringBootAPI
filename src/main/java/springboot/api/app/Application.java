ea7269074edf78ea86ed60b146334f28b5d57194
Ludwik Pluta <Ludwik_Pluta@epam.com>
2024-02-02 13:37:33 +0100

    first commit

---
+++ src/main/java/springboot/api/app/Application.java
@@ -0,0 +1,11 @@
+package springboot.api.app;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
