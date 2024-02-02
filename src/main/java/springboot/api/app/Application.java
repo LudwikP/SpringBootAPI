46b219accb04e7f93a2671aba96266a5c3373d11
Ludwik Pluta <Ludwik_Pluta@epam.com>
2024-02-02 15:32:03 +0100

    all set up

--- src/main/java/springboot/api/app/Application.java
+++ src/main/java/springboot/api/app/Application.java
@@ -1,11 +1,13 @@
 package springboot.api.app;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
 
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
 
+        SpringApplication.run(Application.class, args);
     }
 }
